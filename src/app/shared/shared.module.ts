3cc267991a5d02e96652dcfca2228de96229b06a
hin.phipat <67181278+somhin@users.noreply.github.com>
2020-12-20 22:10:16 +0700

    working on PostComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,16 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '../material/material.module';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent, FooterComponent],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    PostComponent,
+  ],
   imports: [CommonModule, RouterModule, FlexLayoutModule, MaterialModule],
-  exports: [HeaderComponent, SidebarComponent, FooterComponent],
+  exports: [HeaderComponent, SidebarComponent, FooterComponent, PostComponent],
 })
 export class SharedModule {}
